c0c6063fa0ec929ec76b9bd8b206df1603d27d86
diegonicita <diegonicita@gmail.com>
2024-10-10 11:10:45 -0300

    Create Pages and Add Pixi React

---
+++ app/(pages)/create/page.tsx
@@ -0,0 +1,11 @@
+import PixiTest from '../../components/pixiTest'
+
+export default function Page() {
+  return (
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col lg:flex-row">
+        <PixiTest />
+      </div>
+    </div>
+  )
+}
